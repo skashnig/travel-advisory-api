4e757694c1595a10baafbe11e86fada99b7857d4
Savanna Kashnig <slkashni@ncsu.edu>
2026-05-16 21:11:49 -0400

    added comments and news articles list

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ news_key = os.getenv("NEWS_API_KEY")
 @app.get("/advisory/{country_code}")
 def get_advisory(country_code: str):
 
+    # Get geolocation data for the country from the country code
     response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={country_code}&appid={openweather_key}")
     data = response.json()
     print(f"Geo response status: {response.status_code}")
@@ -21,15 +22,20 @@ def get_advisory(country_code: str):
     #lon = data[0]["lon"]
     lat = 40.7128
     lon = -74.0060
+
+    # Get weather data for the location from the lat and lon
     weather_response = requests.get(f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={openweather_key}")
     weather_data = weather_response.json()
-    #  print(weather_data)
-    # weather = weather_data["current"]
+    #print(weather_data)
+    #weather = weather_data["current"]
     weather = "Sunny"
 
+    # Get news data for the country from the country code
     news_response = requests.get(f"https://newsapi.org/v2/top-headlines?country={country_code}&apiKey={news_key}")
     news_data = news_response.json()
     print(news_data)
-    travel_warning = news_data["articles"]
+    travel_warning = []
+    for article in news_data["articles"]:
+        travel_warning.append(article["title"])
 
     return {"country_code": country_code, "travel_warning": travel_warning, "weather": f"{weather}"}
